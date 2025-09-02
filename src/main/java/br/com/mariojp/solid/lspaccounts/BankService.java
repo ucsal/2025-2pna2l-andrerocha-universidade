7f9d7a8550fb3a627faa04b7553e6a12882abc46
200031957 <200031957@CPT-LAMIX-XX.ucsal.br>
2025-09-02 20:20:32 -0300

    Criação do Withdrawable

--- src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
+++ src/main/java/br/com/mariojp/solid/lspaccounts/BankService.java
@@ -1,7 +1,14 @@
 package br.com.mariojp.solid.lspaccounts;
 
 public class BankService {
-    public void processWithdrawal(Account acc, double amount){
-        acc.withdraw(amount);
+
+    public void processWithdrawal(Account account, double amount) {
+        if (account instanceof Withdrawable) {
+            ((Withdrawable) account).withdraw(amount);
+        }
+    }
+
+    public void processDeposit(Account account, double amount) {
+        account.deposit(amount);
     }
 }
